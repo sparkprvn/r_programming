9b868e0d8c00d02be577539df93a20ad4c894edd
Praveen Kumar <35918752+sparkprvn@users.noreply.github.com>
2022-03-08 22:26:26 +0000

    Update basics.R

--- basics.R
+++ basics.R
@@ -10,3 +10,11 @@ days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
 # Assign the names of the day to roulette_vector and poker_vector
 names(poker_vector) <-   days_vector
 names(roulette_vector) <- days_vector
+
+
+###
+A_vector <- c(1, 2, 3)
+B_vector <- c(4, 5, 6)
+
+# Take the sum of A_vector and B_vector
+total_vector <-  A_vector + B_vector
