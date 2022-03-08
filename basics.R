021c5e43e7eee1e2c69535dea147f985582c1203
Praveen Kumar <35918752+sparkprvn@users.noreply.github.com>
2022-03-08 22:28:02 +0000

    Update basics.R

--- basics.R
+++ basics.R
@@ -18,3 +18,14 @@ B_vector <- c(4, 5, 6)
 
 # Take the sum of A_vector and B_vector
 total_vector <-  A_vector + B_vector
+
+####
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Assign to total_daily how much you won/lost on each day
+total_daily <-  roulette_vector + poker_vector
