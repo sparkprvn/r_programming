7f944bde24cc8ca6ae5f88a31743f4c93bd087cc
Praveen Kumar <35918752+sparkprvn@users.noreply.github.com>
2022-03-09 22:33:36 +0000

    Update basics.R

--- basics.R
+++ basics.R
@@ -82,3 +82,15 @@ total_roulette <- sum(roulette_vector)
 
 # Check if you realized higher total gains in poker than in roulette
 total_poker > total_roulette
+
+
+###
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Define a new variable based on a selection
+poker_wednesday <- poker_vector[3]
