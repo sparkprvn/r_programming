4fe13ffe7835b36d5e78a0e15ac2a7375e12a81f
Praveen Kumar <35918752+sparkprvn@users.noreply.github.com>
2022-03-10 22:24:01 +0000

    Update basics.R

--- basics.R
+++ basics.R
@@ -181,3 +181,16 @@ roulette_winning_days <- roulette_vector[selection_vector]
 # Construct a matrix with 3 rows that contain the numbers 1 up to 9
 
 matrix(1:9, byrow = TRUE, nrow = 3)
+
+
+####
+# Box office Star Wars (in millions!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+# Create box_office
+box_office <- c(new_hope, empire_strikes, return_jedi)
+
+# Construct star_wars_matrix
+star_wars_matrix <- matrix(box_office,nrow=3, byrow=TRUE)
